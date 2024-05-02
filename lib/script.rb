4a7d761ea26c6250ebcac8debf18d763326ad004
adrianmihail <adrianmihail00@gmail.com>
2024-05-02 15:52:21 +0300

    Check User input and asign if it matches

--- lib/script.rb
+++ lib/script.rb
@@ -11,6 +11,16 @@ def select_random_word(dictionary)
   random_word
 end
 
+def replace_letters(word_array)
+
+  guess_array = word_array.collect do |element|
+    element = '_'
+  end
+
+  guess_array
+end
+
+
 # import words inot an array from a dictionary
 dictionary_arr = IO.readlines('./dictionary.txt').map(&:chomp)
 
@@ -23,7 +33,35 @@ p random_word_arr
 
 # random_word_arr length
 random_word_length = random_word_arr.length
-p random_word_length
 
 # create array which has the same size as the random_word_arr
-# player will input letters into it to match the random_word_arr
+guess_array = replace_letters(random_word_arr)
+p guess_array
+
+# turn counter
+turns = 0
+
+until guess_array == random_word_arr || turns == 15
+
+  # get user input
+  user_input = gets.chomp
+
+  # check if user input matches a letter from the array
+  if random_word_arr.include?(user_input)
+    letter_index = random_word_arr.each_index.select {|index| random_word_arr[index] == user_input}
+    # if letter matched, input it into position
+    if letter_index.count == 1
+      guess_array[letter_index[0]] = user_input
+    elsif letter_index.count > 1
+      letter_index.each {|index| guess_array[index] = user_input}
+    end
+    p guess_array
+  else
+    puts "Incorrect input! Please provide only 1 letter."
+  end
+
+  # add 1 more turn after input
+  turns += 1
+  # show current turn number
+  puts "Turn number: #{turns}"
+end
